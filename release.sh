2b27fd67ca63d58094e9a9d06d2d56116e367a74
Rickard Helldin <richard_columbus@hotmail.com>
2026-09-26 13:51:03 +0200

    chore: release v0.0.9

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-HARDSHIP_VERSION="${1:-0.0.8}"
+HARDSHIP_VERSION="${1:-0.0.9}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DIST_DIR="$SCRIPT_DIR/dist"
 PACKAGE_DIR="$DIST_DIR/tmp"
